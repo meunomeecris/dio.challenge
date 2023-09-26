b4e67d9993571437b3f41baa00677ef17e88069d
Cris Messias <meunomeecriss@gmail.com>
2023-09-26 15:23:44 +0200

    :art:

--- Challenge-2.playground/Contents.swift
+++ Challenge-2.playground/Contents.swift
@@ -7,16 +7,77 @@ import Foundation
 //Neste desafio, você deverá desenvolver dois projetos no playground usando o Xcode. Em ambos você deverá implementar o Paradigma de Programação Orientada a Objetos e explorar conceitos de Controle de Fluxos, Coleções, Funções, Closures, Enuns, Structs e Concorrência.
 
 
-//Controle de fluxos
-//Write a function in Swift that determines whether a positive integer is prime or not. A prime number is one that is divisible only by 1 and itself.
+//Create a Swift program that converts temperatures between Fahrenheit and Celsius. The program should do the following:
 
-func isPrime(number: Int) -> Bool {
-    if number % number != 0 && number % 1 != 0 {
-        return false
-    } else {
-        return true
-    }
+//MARK: Functions
+
+func fahrenheitToCelsius(fahrenheit: Double) -> Double {
+    return (fahrenheit - 32.0) * 5.0/9.0
 }
 
+fahrenheitToCelsius(fahrenheit: 60)
 
-isPrime(number: 1 0)
+func celsiusToFahrenheit(celsius: Double) -> Double {
+    return (celsius * 5.0/9.0) + 32
+}
+
+celsiusToFahrenheit(celsius: 60)
+
+func chooseAnConvert(_ operation: (Double) -> Double, temperature: Double) -> Double {
+    return operation(temperature)
+}
+
+chooseAnConvert(celsiusToFahrenheit(celsius:), temperature: 30)
+
+
+//MARK: Flux Control
+
+/*For example, assigning grades (A, B, C) based on marks obtained by a student.
+ 
+ if the percentage is above 90, assign grade A
+ if the percentage is above 75, assign grade B
+ if the percentage is above 65, assign grade C*/
+
+
+func grades(note: Int) -> String {
+    var grade: String = ""
+    if note >= 65 && note < 75 {
+        grade = "C"
+    } else if note >= 65 && note < 90 {
+        grade = "B"
+    } else if note >= 90 {
+        grade = "A"
+    } else {
+        grade = "0"
+    }
+    
+    return grade
+}
+
+grades(note: 95)
+
+
+//MARK: Flux Control
+
+//Array
+
+var studens: [String] = ["Ana", "Carlos", "Vanessa", "Vithenso", "Xavier"]
+
+print(studens)
+print(studens.count)
+print(studens.first)
+
+
+var studentsID: Set = [3, 4, 6, 7, 9]
+var studentsID2: Set = [2, 4, 5, 7, 10]
+print(studentsID)
+print(studentsID.insert(11))
+print(studentsID.randomElement())
+var union = studentsID.union(studentsID2)
+print(union.sorted())
+var intersection = studentsID.intersection(studentsID2)
+print(intersection)
+var substracting = studentsID.subtracting(studentsID2)
+print(substracting)
+var symmetricDifference = studentsID.symmetricDifference(studentsID2)
+print(symmetricDifference)
